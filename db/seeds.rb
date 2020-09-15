0b7d14838554a42d31e6b9a61ebfab26cd18f0b1
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2020-09-15 14:01:21 +0300

    schedule options

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 if User.count == 0
   User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 end
 
-if StateColor.count == 0
-  StateColor.create!([
-                         {name: 'Emerald green', color: '2dcd87'},
-                         {name: 'French Rose', color: 'ef608c'},
-                         {name: 'Coral pink', color: 'f98077'},
-                         {name: 'Soft Violet', color: 'b18cda'}])
-end
-
-if Duration.count == 0
-  Duration.create!([{name: '1ч', minutes: '60'},
-                    {name: '1.5ч', minutes: '90'}])
-end
-
-if Format.count == 0
-  Format.create!([{name: 'Очно', online: false},
-                  {name: 'Онлайн', online: true},
-                  {name: 'Skype', online: true}])
-end
+#if StateColor.count == 0
+#  StateColor.create!([
+#                         {name: 'Emerald green', color: '2dcd87'},
+#                         {name: 'French Rose', color: 'ef608c'},
+#                         {name: 'Coral pink', color: 'f98077'},
+#                         {name: 'Soft Violet', color: 'b18cda'}])
+#end
+#
+#if Duration.count == 0
+#  Duration.create!([{name: '1ч', minutes: '60'},
+#                    {name: '1.5ч', minutes: '90'}])
+#end
+#
+#if Format.count == 0
+#  Format.create!([{name: 'Очно', online: false},
+#                  {name: 'Онлайн', online: true},
+#                  {name: 'Skype', online: true}])
+#end
