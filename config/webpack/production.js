46f2e9facd9258ba45b2c12e7246b0e44acecc8a
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2020-09-15 13:16:17 +0300

    schedule options

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
+const { VueLoaderPlugin } = require('vue-loader')
 const environment = require('./environment')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment.toWebpackConfig()
