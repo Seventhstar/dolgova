e8166526b1ad0a47701432ac7215fbcf36577a2b
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2020-09-15 14:02:58 +0300

    schedule options

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,6 +1,4 @@
 class Format < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
 end
